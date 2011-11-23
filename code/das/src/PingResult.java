881acb9c04296eb216456f3bc390c263564d5ebd
Paul Mullen <0705786m@student.gla.ac.uk>
2011-11-23 00:59:47 +0000

    fixed a bug

--- code/das/src/PingResult.java
+++ code/das/src/PingResult.java
@@ -3,4 +3,10 @@ public class PingResult extends TestResult {
 	public PingResult(String result){
 		super(result);
 	}
+
+	@Override
+	protected void parseResult() {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- code/das/src/TraceResult.java
+++ code/das/src/TraceResult.java
@@ -3,4 +3,10 @@ public class TraceResult extends TestResult {
 	public TraceResult(String destination){
 		super(destination);
 	}
+
+	@Override
+	protected void parseResult() {
+		// TODO Auto-generated method stub
+		
+	}
 }
